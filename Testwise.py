ef8933326e9d63023be8d8f347e496f6abd43948
saurabhsood91 <saurabhsood91@gmail.com>
2016-02-20 19:46:48 -0700

    add modal and controller

--- Testwise.py
+++ Testwise.py
@@ -16,7 +16,7 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 Triangle(app)
 
-js = Bundle('bower_components/angular/angular.js', 'scripts/controllers/main.js', 'bower_components/angular-bootstrap/ui-bootstrap.js')
+js = Bundle('bower_components/angular/angular.js', 'scripts/controllers/*.js', 'bower_components/angular-bootstrap/ui-bootstrap-tpls.js')
 assets.register('js', js)
 
 
@@ -58,6 +58,10 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+@app.route('/templates/loggedinmodal.html')
+def static_file():
+    return render_template('loggedinmodal.html')
 
 
 if __name__ == '__main__':
--- static/scripts/controllers/main.js
+++ static/scripts/controllers/main.js
@@ -1,6 +1,19 @@
-var app = angular.module('TestWise', []);
+var app = angular.module('TestWise', ['ui.bootstrap']);
 
-app.controller('MainController', function(){
+app.controller('MainController', ['$uibModal', function($modal){
   var self = this;
   self.yourName = "Karthik";
-});
+
+  self.openLoginDialog = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'templates/loggedinmodal.html',
+      controller: 'LoginModalInstanceCtrl as ctrl',
+      backdrop: 'static'
+    });
+    modalInstance.result.then(function(data){
+      // Set token on scope
+      // self.token = data.token;
+      // $state.go('loggedin');
+    });
+  };
+}]);
